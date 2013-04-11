551f1a77951e71528a9f57a06c760f91ecbb9098
Juan Álvaro Muñoz Naranjo juanalvaro83@gmail.com <>
2013-04-11 17:07:46 +0200

    -Added simulation.sh: starts splitter, gatherer, vlc and a number of peers. all in the same script. -Added stop_simulation.sh to stop a simulation (kills all related processes, but for oggfw). -Added a churn option for the peer based on the weibull (shape 0.4) function. the user can indicate the scale of the function. A scale of 0 means no churn. -Peers do not end their execution after a cluster timeout. -The splitter does not expel peers after a complaint. -Actually, peers do not stop their execution when no churn is requested (use stop_simulation.sh in that case).

---
+++ sim-cluster/simulation.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -x
+
+# Simulates flash-crowd peer churn.
+
+#number_of_blocks=100
+number_of_peers=2
+
+usage() {
+    echo $0
+    echo "Simulates flash-crowd peer churn."
+    echo "Parameters:"
+#    echo "  [-b (number of blocks, $number_of_blocks by default)]"
+    echo "  [-n (number of peers, $number_of_peers by default)]"
+    echo "  [-? (help)]"
+}
+
+echo $0: parsing: $@
+
+while getopts "b:n:?" opt; do
+    case ${opt} in
+	b)
+	    number_of_blocks="${OPTARG}"
+	    ;;
+	n)
+	    number_of_peers="${OPTARG}"
+	    ;;
+	?)
+	    usage
+	    exit 0
+	    ;;
+	\?)
+	    echo "Invalid option: -${OPTARG}" >&2
+	    usage
+	    exit 1
+	    ;;
+	:)
+	    echo "Option -${OPTARG} requires an argument." >&2
+	    usage
+	    exit 1
+	    ;;
+    esac
+done
+
+#clear previous output files
+rm  /home/jalvaro/workspaces-eclipse/P2PSP-sim-cluster/sim/sim-cluster/output/*
+
+#xterm -e "./splitter.py --block_size=$block_size --channel=$source_channel --source_hostname=$source_hostname --source_port=$source_port --listening_port=$splitter_port" &
+
+#start the splitter
+xterm -l -lf ./output/salida_splitter.txt -e "./splitter.py --source_hostname=localhost" &
+
+sleep 1
+
+#start the gatherer
+#xterm -e "./gatherer.py --buffer_size=$buffer_size --listening_port=$[splitter_port+1] --channel=$source_channel --source_hostname=$source_hostname --source_port=$source_port --splitter_hostname=$splitter_hostname --splitter_port=$splitter_port" &
+xterm -l -lf ./output/salida_gatherer.txt -e "./gatherer.py --splitter_hostname=localhost" &
+
+sleep 1
+
+#start the player
+vlc http://localhost:9999 &
+
+sleep 10
+
+COUNTER=0
+while [ $COUNTER -lt $number_of_peers ];
+do
+    ./peer.py --splitter_hostname=localhost --no_player --logging_level=DEBUG --logging_file=./output/peer-${COUNTER}  --churn=0 &
+    let COUNTER=COUNTER+1 
+done
+
+set +x
---
+++ sim-cluster/stop_simulation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Kill every related process. Don't kill oggfw!
+pkill splitter
+pkill gatherer
+pkill vlc
+pkill peer
