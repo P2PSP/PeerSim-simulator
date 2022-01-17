f1f3d8805f2a60dbe61746074fa8dab0f2c5f7ac
Gleb Naumenko <naumenko.gs@gmail.com>
2022-01-17 16:47:05 +0200

    Improve results printing

--- src/InvObserver.java
+++ src/InvObserver.java
@@ -56,21 +56,35 @@ public class InvObserver implements Control
 		pid = Configuration.getPid(name + "." + PAR_PROT);
 	}
 
+	public enum Protocol {
+		ERLAY,
+		LEGACY,
+	}
+	public enum NodeType {
+		REACHABLE,
+		PRIVATE,
+	}
+
 	public boolean execute() {
-		// Track how many invs were sent. Reachable nodes are tracked by [0], private are tracked
-		// by [1].
-		int[] invsSent = new int[2];
-		int[] shortInvsSent = new int[2];
-		int[] txSent = new int[2];
+		// Track how many invs and txs were sent.
+		HashMap<Protocol, Integer> invsByProtocol = new HashMap<>();
+		HashMap<Protocol, Integer> txsByProtocol = new HashMap<>();
+		HashMap<NodeType, Integer> invsByNodeType = new HashMap<>();
+		HashMap<NodeType, Integer> txsByNodeType = new HashMap<>();
+		HashMap<NodeType, Integer> shortInvsByNodeType = new HashMap<>();
+
 		// Track reconciliation results across experiments.
 		ArrayList<Integer> successRecons = new ArrayList<>();
 		ArrayList<Integer> failedRecons = new ArrayList<>();
 		// Track how soon transactions were propagating across the network.
 		HashMap<Integer, ArrayList<Long>> txArrivalTimes = new HashMap<Integer, ArrayList<Long>>();
-		int blackHoles = 0, reachableNodes = 0;
+		int blackHoles = 0, reconcilingNodes = 0, reachableNodes = 0;
+
 		for(int i = 1; i < Network.size(); i++) {
 			Peer peer = (Peer) Network.get(i).getProtocol(pid);
 
+			// Store all arrival times (at every node) for all transactions. We will later use this
+			// to calculate latency.
 			Iterator it = peer.txArrivalTimes.entrySet().iterator();
 			while (it.hasNext()) {
 				Map.Entry pair = (Map.Entry)it.next();
@@ -82,24 +96,44 @@ public class InvObserver implements Control
 				txArrivalTimes.get(txId).add(arrivalTime);
 			}
 
+			// See how many black holes there were.
 			if (peer.isBlackHole) {
 				++blackHoles;
 				continue;
 			}
 
 			if (peer.isReachable) {
-				invsSent[0] += peer.invsSent;
-				shortInvsSent[0] += peer.shortInvsSent;
-				txSent[0] += peer.txSent;
-				++reachableNodes;
+				invsByNodeType.put(NodeType.REACHABLE, invsByNodeType.getOrDefault(NodeType.REACHABLE, 0) + peer.invsSent);
+				txsByNodeType.put(NodeType.REACHABLE, txsByNodeType.getOrDefault(NodeType.REACHABLE, 0) + peer.txSent);
+				reachableNodes++;
 			} else {
-				invsSent[1] += peer.invsSent;
-				shortInvsSent[1] += peer.shortInvsSent;
-				txSent[1] += peer.txSent;
+				invsByNodeType.put(NodeType.PRIVATE, invsByNodeType.getOrDefault(NodeType.PRIVATE, 0) + peer.invsSent);
+				txsByNodeType.put(NodeType.PRIVATE, txsByNodeType.getOrDefault(NodeType.PRIVATE, 0) + peer.txSent);
 			}
 
-			successRecons.add(peer.successRecons);
-			failedRecons.add(peer.failedRecons);
+			// See how many inv/shortinv/tx messages every node sent.
+			if (peer.reconcile) {
+				invsByProtocol.put(Protocol.ERLAY, invsByProtocol.getOrDefault(Protocol.ERLAY, 0) + peer.invsSent);
+				txsByProtocol.put(Protocol.ERLAY, txsByProtocol.getOrDefault(Protocol.ERLAY, 0) + peer.txSent);
+				reconcilingNodes++;
+				successRecons.add(peer.successRecons);
+				failedRecons.add(peer.failedRecons);
+
+				if (peer.isReachable) {
+					shortInvsByNodeType.put(NodeType.REACHABLE, shortInvsByNodeType.getOrDefault(NodeType.REACHABLE, 0) + peer.shortInvsSent);
+				} else {
+					shortInvsByNodeType.put(NodeType.PRIVATE, shortInvsByNodeType.getOrDefault(NodeType.PRIVATE, 0) + peer.shortInvsSent);
+				}
+			} else {
+				invsByProtocol.put(Protocol.LEGACY, invsByProtocol.getOrDefault(Protocol.LEGACY, 0) + peer.invsSent);
+				txsByProtocol.put(Protocol.LEGACY, txsByProtocol.getOrDefault(Protocol.LEGACY, 0) + peer.txSent);
+			}
+		}
+
+		int allTxs = txArrivalTimes.size();
+
+		if (allTxs == 0) {
+			return false;
 		}
 
 		// Measure the delays it took to reach majority of the nodes (based on receival time).
@@ -127,38 +161,40 @@ public class InvObserver implements Control
 			avgTxArrivalDelay.add(percentile95delay);
 		}
 
-		// Print results.
-		int allTxs = txArrivalTimes.size();
-
-		if (allTxs == 0) {
-			return false;
-		}
-
+		System.err.println("");
+		System.err.println("");
+		System.err.println("");
+		System.err.println("-----------RESULTS--------");
 		System.err.println("Relayed txs: " + allTxs);
 
 		double avgMaxDelay = avgTxArrivalDelay.stream().mapToLong(val -> val).average().orElse(0.0);
 		System.out.println("Avg max latency: " + avgMaxDelay);
 
-		if (blackHoles == 0) {
+		if (blackHoles != 0) {
 			// Note that black holes are only useful to measure latency
 			// impact, measuring/comparing bandwidth is currently not supported because it depends
 			// on how exactly black holes operate (do they reconcile with empty sketches? or drop
 			// sketches/requests on the floor?).
+			return false;
+		}
+		System.out.println("Total bandwidth per tx");
+		int shortInvsTotal = shortInvsByNodeType.getOrDefault(NodeType.REACHABLE, 0) + shortInvsByNodeType.getOrDefault(NodeType.PRIVATE, 0);
+		int invsTotal = invsByNodeType.get(NodeType.REACHABLE) + invsByNodeType.get(NodeType.PRIVATE);
+		System.out.println("INV items: " + (invsTotal + shortInvsTotal * 0.25)  / allTxs / (Network.size() - 1));
 
-			System.out.println("Total bandwidth per tx:");
-			System.out.println("INV items: " + (invsSent[0] + invsSent[1] +
-				(shortInvsSent[0] + shortInvsSent[1]) * 0.25) / allTxs / (Network.size() - 1));
-			System.out.println("Of them short invs: " + ((shortInvsSent[0] + shortInvsSent[1]) * 0.25) / allTxs / (Network.size() - 1));
-			System.out.println("TX items: " + (txSent[0] + txSent[1]) * 1.0 / allTxs / (Network.size() - 1));
-
-			System.out.println("An average reachable node spends the following bandwidth:");
-			System.out.println("INV items: " + (invsSent[0] + shortInvsSent[0] * 0.25));
-			System.out.println("TX items: " + txSent[0]);
-
-			int privateNodes = Network.size() - 1 - reachableNodes;
-			System.out.println("An average private node spends the following bandwidth:");
-			System.out.println("INV items: " + (invsSent[1] + shortInvsSent[1] * 0.25));
-			System.out.println("TX items: " + txSent[1]);
+		System.out.println("");
+		System.out.println("Total bandwidth per tx based on the protocol");
+		int legacyNodes = Network.size() - reconcilingNodes - 1;
+		if (legacyNodes > 0) {
+			System.out.println("Legacy:");
+			System.out.println("INV items: " + (invsByProtocol.get(Protocol.LEGACY) * 1.0 / allTxs / legacyNodes));
+			System.out.println("TX items: " + (txsByProtocol.get(Protocol.LEGACY) * 1.0 / allTxs / legacyNodes));
+		}
+		if (reconcilingNodes > 0) {
+			System.out.println("Reconciling:");
+			System.out.println("INV items: " + (invsByProtocol.get(Protocol.ERLAY) + shortInvsTotal * 0.25) / allTxs / reconcilingNodes);
+			System.out.println("Of them short invs: " + (shortInvsTotal * 0.25 / allTxs / reconcilingNodes));
+			System.out.println("TX items: " + (txsByProtocol.get(Protocol.ERLAY) * 1.0 / allTxs / reconcilingNodes));
 
 			double avgSuccessRecons = successRecons.stream().mapToInt(val -> val).average().orElse(0.0);
 			if (avgSuccessRecons > 0) {
@@ -168,6 +204,17 @@ public class InvObserver implements Control
 				System.out.println(avgFailedRecons + " failed recons on average.");
 			}
 		}
+
+		System.out.println("");
+		System.out.println("Total bandwidth per tx based on reachability");
+		int privateNodes = Network.size() - reachableNodes - 1;
+		System.out.println("Reachable:");
+		System.out.println("INV items: " + (invsByNodeType.get(NodeType.REACHABLE) * 1.0 / allTxs / reachableNodes));
+		System.out.println("TX items: " + (txsByNodeType.get(NodeType.REACHABLE) * 1.0 / allTxs / reachableNodes));
+
+		System.out.println("Private:");
+		System.out.println("INV items: " + (invsByNodeType.get(NodeType.PRIVATE) * 1.0 / allTxs / privateNodes));
+		System.out.println("TX items: " + (txsByNodeType.get(NodeType.PRIVATE) * 1.0 / allTxs / privateNodes));
 		System.err.println("");
 		return false;
 	}
